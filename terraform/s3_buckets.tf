08de6d7c481051fff6a81a3b63a2f6fbf3ff0428
oleksiiredko <oredko@contractor.worldremit.com>
2023-01-30 11:10:48 +0200

    added s3 dst bucket

--- terraform/s3_buckets.tf
+++ terraform/s3_buckets.tf
@@ -1,15 +1,32 @@
-resource "aws_s3_bucket" "private_bucket" {
-  bucket = "oredko-s3-side-bucket"
+# Source bucket
+resource "aws_s3_bucket" "private_bucket_src" {
+  bucket = "oredko-src-bucket"
 }
-
-resource "aws_s3_bucket_acl" "bucket_acl" {
-  bucket = aws_s3_bucket.private_bucket.id
+resource "aws_s3_bucket_acl" "bucket_acl_src" {
+  bucket = aws_s3_bucket.private_bucket_src.id
   acl    = "private"
 }
-
-resource "aws_s3_bucket_versioning" "bucket_versioning" {
-  bucket = aws_s3_bucket.private_bucket.id
+resource "aws_s3_bucket_versioning" "bucket_versioning_src" {
+  bucket = aws_s3_bucket.private_bucket_src.id
   versioning_configuration {
     status = "Disabled"
   }
 }
+
+# Destination bucket
+resource "aws_s3_bucket" "private_bucket_dst" {
+  bucket = "oredko-dst-bucket"
+}
+
+resource "aws_s3_bucket_acl" "bucket_dst" {
+  bucket = aws_s3_bucket.private_bucket_dst.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "bucket_versioning_dst" {
+  bucket = aws_s3_bucket.private_bucket_dst.id
+  versioning_configuration {
+    status = "Disabled"
+  }
+}
+
